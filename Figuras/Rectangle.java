4c84bce2290f289511ba69fd8adaecac3e8cdf35
muimuiasd <muimuiasd@gmail.com>
2017-07-01 14:49:21 -0400

    Figuras

---
+++ Figuras/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle
+{
+  int x;
+  int y;
+  int width;
+  int height;
+  String fill;
+  String strokewidth;
+  String stroke;
+
+  public Rectangle (x, y, w, h, f, sw, s)
+  {
+    
+  }
+}
