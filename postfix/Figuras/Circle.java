60736d86f12fe2b21e0d52d4bdc7bce95cd9df2a
CamilaBadwi <camilapinto@outlook.cl>
2017-07-01 15:05:20 -0400

    Circle & Line

--- postfix/Figuras/Circle.java
+++ postfix/Figuras/Circle.java
@@ -1,4 +1,3 @@
-
 public class Circle {
   int cx;
   int cy;
---
+++ postfix/Figuras/Line.java
@@ -0,0 +1,19 @@
+public class Line {
+  int x1;
+  int x2;
+  int y1;
+  int y2;
+  int strokewidth;
+  String stroke;
+
+  public Line (int x1, int x2, int y1, int y2, int sw, String s) {
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
+    strokewidth = sw;
+    stroke = s;
+
+  }
+  
+}
