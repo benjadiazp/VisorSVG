712a319a32524275db73fdb56860e7d68ccd149a
CamilaBadwi <camilapinto@outlook.cl>
2017-07-01 15:21:22 -0400

    Ellipse Polygon Polyline

---
+++ postfix/Figuras/Ellipse.java
@@ -0,0 +1,20 @@
+public class Ellipse {
+  int cx;
+  int cy;
+  int rx;
+  int ry;
+  int strokewidth;
+  String fill;
+  String stroke;
+
+  public Ellipse (int cx, int cy, int rx, int ry, int sw, String f, String s){
+    this.cx = cx;
+    this.cy = cy;
+    this.rx = rx;
+    this.ry = ry;
+    strokewidth = sw;
+    fill = f;
+    stroke = s;
+
+  }
+}
---
+++ postfix/Figuras/Polygon.java
@@ -0,0 +1,15 @@
+public class Polygon {
+  int points;
+  int strokewidth;
+  String fill;
+  String stroke;
+
+  public Polygon (int p, int sw, String f, String s){
+    points = p;
+    strokewidth = sw;
+    fill = f;
+    stroke = s;
+
+  }
+  
+}
