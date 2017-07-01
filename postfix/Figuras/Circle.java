de214bcf4ba0d0fa5699ff68502e5f9c291167c0
CamilaBadwi <camilapinto@outlook.cl>
2017-07-01 14:57:14 -0400

    Circle

--- postfix/Figuras/Circle.java
+++ postfix/Figuras/Circle.java
@@ -1,3 +1,21 @@
+
 public class Circle {
+  int cx;
+  int cy;
+  int r;
+  int strokewidth;
+  String stroke;
+  String fill;
+
+
+  public Circle (int cx, int cy, int r, String sw, String s, String f) {
+    this.cx = cx;
+    this.cy = cy;
+    this.r = r;
+    strokewidth = sw;
+    stroke = s;
+    fill = f;
+
+  }
 
 }
