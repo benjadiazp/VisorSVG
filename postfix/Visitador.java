8b43ee36282c7d5c36b09ef9bf9995fe17824942
sidgim <gaston.lara.p@gmail.com>
2017-07-08 23:51:49 -0400

    visitador 2

--- postfix/Visitador.java
+++ postfix/Visitador.java
@@ -1,6 +1,90 @@
 import postfix.node.*;
 import postfix.DepthFirstAdapter.*;
+import java.util.LinkedList;
 
 public class Visitador extends DepthFirstAdapter {
-  
+  public LinkedList<String> lista = new LinkedList<>();
+
+  @Override
+
+  public void caseALineF(ALineF node)
+  {
+      inALineF(node);
+      if(node.getLine() != null)
+      {
+          node.getLine().apply(this);
+      }
+      if(node.getL1() != null)
+      {
+          node.getL1().apply(this);
+      }
+      outALineF(node);
+  }
+  public void caseACircleF(ACircleF node)
+  {
+      inACircleF(node);
+      if(node.getCircle() != null)
+      {
+          node.getCircle().apply(this);
+      }
+      if(node.getC1() != null)
+      {
+          node.getC1().apply(this);
+      }
+      outACircleF(node);
+  }
+  public void caseARectF(ARectF node)
+  {
+      inARectF(node);
+      if(node.getRect() != null)
+      {
+          node.getRect().apply(this);
+      }
+      if(node.getR1() != null)
+      {
+          node.getR1().apply(this);
+      }
+      outARectF(node);
+  }
+  public void caseAEllipseF(AEllipseF node)
+  {
+      inAEllipseF(node);
+      if(node.getEllipse() != null)
+      {
+          node.getEllipse().apply(this);
+      }
+      if(node.getE1() != null)
+      {
+          node.getE1().apply(this);
+      }
+      outAEllipseF(node);
+  }
+  public void caseAPolygonF(APolygonF node)
+  {
+      inAPolygonF(node);
+      if(node.getPolygon() != null)
+      {
+          node.getPolygon().apply(this);
+      }
+      if(node.getP() != null)
+      {
+          node.getP().apply(this);
+      }
+      outAPolygonF(node);
+  }
+  public void caseAPolylineF(APolylineF node)
+  {
+      inAPolylineF(node);
+      if(node.getPolyline() != null)
+      {
+          node.getPolyline().apply(this);
+      }
+      if(node.getA() != null)
+      {
+          node.getA().apply(this);
+      }
+      outAPolylineF(node);
+  }
+
+
 }
